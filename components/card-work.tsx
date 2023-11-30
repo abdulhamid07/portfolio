ea2d8531d939289e96a673b879f2d323ae846ca9
abdulhamid07 <abdulhamid.dev@gmail.com>
2023-11-30 15:45:32 +0700

    optimize card-work and card-slider

--- components/card-work.tsx
+++ components/card-work.tsx
@@ -12,7 +12,10 @@ interface CardWorkProps {
 const CardWork = ({ work }: CardWorkProps) => {
   const { onOpen } = useDetailWork();
   return (
-    <div className="relative rounded-lg overflow-hidden flex items-center justify-center group">
+    <div
+      onClick={() => onOpen(work)}
+      className="relative rounded-lg overflow-hidden flex items-center justify-center group"
+    >
       <div className="flex items-center justify-center relative overflow-hidden group">
         {/* image */}
         <Image
@@ -26,10 +29,7 @@ const CardWork = ({ work }: CardWorkProps) => {
         {/* overlay gradient */}
         <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
         {/* title */}
-        <div
-          onClick={() => onOpen(work)}
-          className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300"
-        >
+        <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
           <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
             <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
               Detail
--- components/work-slider.tsx
+++ components/work-slider.tsx
@@ -8,7 +8,6 @@ import "swiper/css/pagination";
 import { slideType } from "@/app/work/page";
 import { Pagination } from "swiper";
 import CardWork from "./card-work";
-import { Button } from "./ui/button";
 import { BsArrowRight } from "react-icons/bs";
 
 interface WorkSliderProps {
@@ -40,21 +39,8 @@ const WorkSlider = ({ works }: WorkSliderProps) => {
                 }
                 className="group relative rounded-lg overflow-hidden flex items-center justify-center bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 hover:bg-gradient-to-tl text-white border-0"
               >
-                <Button
-                  variant="link"
-                  className="text-white no-underline"
-                  onClick={() =>
-                    window.open(
-                      "https://abdulhamid07.github.io/portfolio.html",
-                      "_blank"
-                    )
-                  }
-                >
-                  <span className="font-light tracking-widest">
-                    See Other Projects
-                  </span>
-                </Button>
-                <div className="text-xl translate-x-[500%] group-hover:translate-x-0 transition-all duration-300 delay-200">
+                <span className="text-sm font-light text-center">See Others</span>
+                <div className="ml-1 text-xl translate-x-[800%] group-hover:translate-x-0 transition-all duration-300 delay-200">
                   <BsArrowRight />
                 </div>
               </div>
